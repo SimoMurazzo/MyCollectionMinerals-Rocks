cfe502217eafa14083ac496ab89767c1b03119d5
simone <murazzosimone@gmail.com>
2024-12-28 12:59:00 +0100

    Working advanced search and search button disabled when fields are empty

--- Minerals/views.py
+++ Minerals/views.py
@@ -9,12 +9,41 @@ class MineralList(ListView):
     paginate_by = 12
 
     def get_queryset(self):
+        total_query = Q()
+        locality = self.request.GET.get('locality')
+        if locality:
+            total_query = total_query & Q(locality__icontains=locality)
+        rarity = self.request.GET.get('rarity')
+        if rarity:
+            total_query = total_query & Q(rarity__icontains=rarity)
+        environment = self.request.GET.get('environment')
+        if environment:
+            total_query = total_query & Q(environment__icontains=environment)
+        crystal_system = self.request.GET.get('crystal_system')
+        if crystal_system:
+            total_query = total_query & Q(crystal_system__icontains=crystal_system)
+        cleavage = self.request.GET.get('cleavage')
+        if cleavage:
+            total_query = total_query & Q(cleavage__icontains=cleavage)
+        luminescence = self.request.GET.get('luminescence')
+        if luminescence:
+            total_query = total_query & Q(luminescence__icontains=luminescence)
+        luster = self.request.GET.get('luster')
+        if luster:
+            total_query = total_query & Q(luster__icontains=luster)
+        streak = self.request.GET.get('streak')
+        if streak:
+            total_query = total_query & Q(streak__icontains=streak)
+        pleochroism = self.request.GET.get('pleochroism')
+        if pleochroism:
+            total_query = total_query & Q(pleochroism__icontains=pleochroism)
+        radioactivity = self.request.GET.get('radioactivity')
+        if radioactivity:
+            total_query = total_query & Q(radioactivity__icontains=radioactivity)
         query = self.request.GET.get('query')
-        print(query)
         if query:
-            return Mineral.objects.filter(Q(name__icontains=query) | Q(description__icontains=query) | Q(strunz_class=query))
-        else:
-            return Mineral.objects.all()
+            total_query = total_query & (Q(name__icontains=query) | Q(description__icontains=query) | Q(strunz_class__icontains=query))
+        return Mineral.objects.filter(total_query).order_by('strunz_class')
 
 
 class MineralDetail(DetailView):
