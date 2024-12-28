691ea0cfd7c97840ebe20ee06898fcb9db1b3982
simone <murazzosimone@gmail.com>
2024-12-28 16:43:59 +0100

    Rename

--- MyCollectionMinerals-Rocks/urls.py
+++ MyCollectionMinerals-Rocks/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for MyMineralCollection project.
+URL configuration for MyCollectionMinerals-Rocks project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- MyCollectionMinerals-Rocks/wsgi.py
+++ MyCollectionMinerals-Rocks/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MyMineralCollection project.
+WSGI config for MyCollectionMinerals-Rocks project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyMineralCollection.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyCollectionMinerals-Rocks.settings')
 
 application = get_wsgi_application()
